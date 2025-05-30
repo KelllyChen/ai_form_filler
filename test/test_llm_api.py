a00aba89f3be63c6a136303d46b0b57e970cc4c5
Karen1003 <k96320@gmail.com>
2025-05-29 23:37:44 -0400

    test scripts

---
+++ test/test_llm_api.py
@@ -0,0 +1,7 @@
+from llm_api import query_llm
+import os
+
+def test_mock_response(monkeypatch):
+    monkeypatch.setenv("USE_MOCK", "true")
+    result = query_llm("Generate a value for the form field: 'summary_about_you'")
+    assert "(Mocked" in result
